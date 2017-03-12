a959ebd6a7ff9709c219d606edf9942fa5f99d7e
Donald Pinckney <donald_pinckney@icloud.com>
2017-03-11 21:51:43 -0800

    Move functions to new file, implement if else

--- evaluation.hs
+++ evaluation.hs
@@ -4,6 +4,7 @@ import Data.Maybe
 
 import DataTypes
 import Errors
+import Functions
 
 eval :: WVal -> ThrowsError WVal
 eval val@(String _) = return val
@@ -28,90 +29,3 @@ convertListOp typeList =
   if length typeList > 2 then
     if all (== (head typeList)) typeList then take 2 typeList else typeList
   else typeList
-
-
-
-
-funcTable :: [(FuncDef, [WVal] -> WVal)]
-funcTable =
-    [
-    (("+", [TIntegral, TIntegral]), integerBinaryOp (+)),
-    (("+", [TFloat, TFloat]), floatBinaryOp (+)),
-    (("-", [TIntegral, TIntegral]), integerBinaryOp (-)),
-    (("-", [TFloat, TFloat]), floatBinaryOp (-)),
-    (("-", [TIntegral]), integerUnaryOp (0-)),
-    (("-", [TFloat]), floatUnaryOp (0-)),
-    (("*", [TIntegral, TIntegral]), integerBinaryOp (*)),
-    (("*", [TFloat, TFloat]), floatBinaryOp (*)),
-    (("/", [TIntegral, TIntegral]), integerBinaryOp quot),
-    (("/", [TFloat, TFloat]), floatBinaryOp (/)),
-    (("=", [TIntegral, TIntegral]), integerBinaryBoolOp (==)),
-    (("<", [TIntegral, TIntegral]), integerBinaryBoolOp (<)),
-    ((">", [TIntegral, TIntegral]), integerBinaryBoolOp (>)),
-    (("/=", [TIntegral, TIntegral]), integerBinaryBoolOp (/=)),
-    (("<=", [TIntegral, TIntegral]), integerBinaryBoolOp (<=)),
-    ((">=", [TIntegral, TIntegral]), integerBinaryBoolOp (>=)),
-    (("=", [TFloat, TFloat]), floatBinaryBoolOp (==)),
-    (("<", [TFloat, TFloat]), floatBinaryBoolOp (<)),
-    ((">", [TFloat, TFloat]), floatBinaryBoolOp (>)),
-    (("/=", [TFloat, TFloat]), floatBinaryBoolOp (/=)),
-    (("<=", [TFloat, TFloat]), floatBinaryBoolOp (<=)),
-    ((">=", [TFloat, TFloat]), floatBinaryBoolOp (>=)),
-    (("=", [TString, TString]), stringBinaryBoolOp (==)),
-    (("<", [TString, TString]), stringBinaryBoolOp (<)),
-    ((">", [TString, TString]), stringBinaryBoolOp (>)),
-    (("/=", [TString, TString]), stringBinaryBoolOp (/=)),
-    (("<=", [TString, TString]), stringBinaryBoolOp (<=)),
-    ((">=", [TString, TString]), stringBinaryBoolOp (>=)),
-    (("&&", [TBool, TBool]), boolBinaryOp (&&)),
-    (("||", [TBool, TBool]), boolBinaryOp (||)),
-    (("not", [TBool]), boolUnaryOp not)
-    ]
-
-
--- Math Arithmetic Function Types
-integerUnaryOp :: (Integer -> Integer) -> [WVal] -> WVal
-integerUnaryOp op = Integral . op . unpackInteger . head
-
-integerBinaryOp :: (Integer -> Integer -> Integer) -> [WVal] -> WVal
-integerBinaryOp op params = Integral $ foldl1 op $ map unpackInteger params
-
-floatUnaryOp :: (Double -> Double) -> [WVal] -> WVal
-floatUnaryOp op = Float . op . unpackFloat . head
-
-floatBinaryOp :: (Double -> Double -> Double) -> [WVal] -> WVal
-floatBinaryOp op params = Float $ foldl1 op $ map unpackFloat params
-
-
--- Boolean Arithmetic Function Types
-boolUnaryOp :: (Bool -> Bool) -> [WVal] -> WVal
-boolUnaryOp op = Bool . op . unpackBool . head
-
-boolBinaryOp :: (Bool -> Bool -> Bool) -> [WVal] -> WVal
-boolBinaryOp op params = Bool $ foldl1 op $ map unpackBool params
-
-
--- Comparision Function Types
-integerBinaryBoolOp :: (Integer -> Integer -> Bool) -> [WVal] -> WVal
-integerBinaryBoolOp op params = Bool $ foldl (\b (x, y) -> b && (op x y)) True $ zip unpackedParams (tail unpackedParams)
-    where unpackedParams = map unpackInteger params
-
-floatBinaryBoolOp :: (Double -> Double -> Bool) -> [WVal] -> WVal
-floatBinaryBoolOp op params = Bool $ foldl (\b (x, y) -> b && (op x y)) True $ zip unpackedParams (tail unpackedParams)
-    where unpackedParams = map unpackFloat params
-
-stringBinaryBoolOp :: (String -> String -> Bool) -> [WVal] -> WVal
-stringBinaryBoolOp op params = Bool $ foldl (\b (x, y) -> b && (op x y)) True $ zip unpackedParams (tail unpackedParams)
-    where unpackedParams = map unpackString params
-
-unpackInteger :: WVal -> Integer
-unpackInteger (Integral n) = n
-
-unpackFloat :: WVal -> Double
-unpackFloat (Float f) = f
-
-unpackBool :: WVal -> Bool
-unpackBool (Bool b) = b
-
-unpackString :: WVal -> String
-unpackString (String s) = s
