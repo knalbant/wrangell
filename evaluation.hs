bea2c9696851e10e105363cffe599ab0b4958cf3
Kenan Nalbant <kanalbant@gmail.com>
2017-03-16 03:10:07 -0700

    Small bits of cleanup in evaluation.hs

--- evaluation.hs
+++ evaluation.hs
@@ -30,32 +30,9 @@ eval env (List [Atom "define", Atom var, form]) =
 eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
 
 
-{-- apply :: String -> [WVal] -> ThrowsError WVal
-apply func args = if isNothing foundFunc
-    then throwError badFuncError
-    else return (fromJust foundFunc args)
-
-    where types = convertListOp $ map getType args
-          badFuncError = NotFunction (func, types)
-          foundFunc = lookup func funcTable
-
-
---}
-
 apply :: String -> [WVal] -> ThrowsError WVal
 apply func args =
     if isNothing funcDef
       then throwError $ NotFunction "Unrecognized primitive function args" func
       else fromJust funcDef args
     where funcDef = lookup func funcTable
-
-
-{--
---a hacky fix to have wrangell work with lists of the same type
-convertListOp :: [WType] -> [WType]
-convertListOp typeList =
-  if length typeList > 2 && (all (== head typeList) typeList)
-    then take 2 typeList
-    else typeList
-
---}
